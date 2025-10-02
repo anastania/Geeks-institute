7dd4222f9c4ac16ebaa81f08d3e3005e1d032f10
anas serghini <anasserghini00@gmail.com>
2025-10-02 16:16:23 +0000

    day 2 week7

---
+++ week7/day2/exercice_xp/react-exercises/src/Components/Phone.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Phone() {
+  const [phone, setPhone] = useState({
+    brand: "Samsung",
+    model: "Galaxy S20",
+    color: "black",
+    year: 2020,
+  });
+
+  const changeColor = () => {
+    setPhone({ ...phone, color: "blue" });
+  };
+
+  return (
+    <div>
+      <h1>My phone</h1>
+      <h2>Brand: {phone.brand}</h2>
+      <h2>Model: {phone.model}</h2>
+      <h2>Color: {phone.color}</h2>
+      <h2>Year: {phone.year}</h2>
+
+      <button onClick={changeColor}>Change Color</button>
+    </div>
+  );
+}
+
+export default Phone;
